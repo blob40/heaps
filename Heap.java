79085d250a6c3c7d88a4b681355a4998332423ab
blob40 <alexandrialax40@gmail.com>
2026-02-26 01:47:34 +0000

    g

--- Heap.java
+++ Heap.java
@@ -1,107 +1,94 @@
 public class Heap {
 
-//the actual storage structure for your heap
+    // the actual storage structure for your heap
 
-private int[] arr;
-public int lastThing = 0;
+    private int[] arr;
+    public int lastThing = 0;
 
-public Heap() {
-arr = new int[100];
-}
-
-//pre condition: toAdd is not null and is an int
-//post condition: toAdd is added to the heap and the heap is re-heapafied
-public void add(int toAdd) {
-    System.out.println("Adding " + toAdd);
-         arr[lastThing] = toAdd;
-         lastThing++;
-         siftUp(lastThing - 1);
-}
-
- 
-
-//remove the largest element of the heap (the root) and re-heapafy
-
-//5 points
-
-//pre condition: the heap is not empty
-//post condition: the largest element of the heap is removed and the heap is re-heap
-public void removeMax() {
-    System.out.println("Removing " + arr[0]);
-    arr[0] = arr[lastThing - 1];
-    System.out.println("Putting " + arr[0] + " at the root");
-    lastThing--;
-    siftDown(0);
-}
-
- 
-
-//this should check and alter the tree after an item is inserted
-
-//3 points
-
-//pre condition: index is the index of the item that was just added to the heap
-//post condition: the heap is re-heapafied
-private void siftUp(int index) {
-
-    if (index == 0){
-        return;
+    public Heap() {
+        arr = new int[100];
     }
-    int parent = arr[(index - 1) / 2];
-    int leftC = arr[((2*index) + 1)];
-    int rightC = arr[((2*index) + 2)];
-    int temp = arr[index];
 
-    if (arr[index] > parent){
-        System.out.println("Swapping " + arr[index] + " and " + parent);
-        arr[index] = parent;
-        arr[(index - 1) / 2] = temp;
-        siftUp((index - 1) / 2);
+    // pre condition: toAdd is not null and is an int
+    // post condition: toAdd is added to the heap and the heap is re-heapafied
+    public void add(int toAdd) {
+        System.out.println("Adding " + toAdd);
+        arr[lastThing] = toAdd;
+        lastThing++;
+        siftUp(lastThing - 1);
     }
-}
 
- 
+    // remove the largest element of the heap (the root) and re-heapafy
 
-//this should check and alter the tree after an item is deleted.
+    // 5 points
 
-//3 points
-
-//pre condition: index is the index of the item that was just moved to the root of the heap
-//post condition: the heap is re-heapafied
-private void siftDown(int index) {
-    System.out.println("Sifting down " + arr[index]);
-   
-    int parent = arr[(index - 1) / 2];
-    int leftC = arr[((2*index) + 1)];
-    int rightC = arr[((2*index) + 2)];
-
-    if (arr[index] < leftC && leftC > rightC){
-        System.out.println("Delete Swapping " + arr[index] + " and " + leftC);
-        arr[index] = leftC;
-        arr[((2*index) + 1)] = parent;
-        siftDown(((2*index) + 1));
-    } else if  (arr[index] < rightC && rightC > leftC){
-        System.out.println("Delete Swapping " + arr[index] + " and " + rightC);
-        arr[index] = rightC;
-        arr[((2*index) + 2)] = parent;
-        siftDown(((2*index) + 2));
+    // pre condition: the heap is not empty
+    // post condition: the largest element of the heap is removed and the heap is
+    // re-heap
+    public void removeMax() {
+        System.out.println("Removing " + arr[0]);
+        arr[0] = arr[lastThing - 1];
+        System.out.println("Putting " + arr[0] + " at the root");
+        lastThing--;
+        siftDown(0);
     }
-}
 
+    // this should check and alter the tree after an item is inserted
 
+    // 3 points
 
- 
+    // pre condition: index is the index of the item that was just added to the heap
+    // post condition: the heap is re-heapafied
+    private void siftUp(int index) {
 
-//4 points for syntax conventions.
-//main method to test your heap
- public static void main(String[] args){
-    int [] nums = {10, 6, 7, 1,2};
-    Heap h = new Heap();
-    for (int i = 0; i < nums.length; i++){
-        h.add(nums[i]);
+        if (index == 0) {
+            return;
+        }
+        int parent = arr[(index - 1) / 2];
+        int leftC = arr[((2 * index) + 1)];
+        int rightC = arr[((2 * index) + 2)];
+        int temp = arr[index];
+
+        if (arr[index] > parent) {
+            System.out.println("Swapping " + arr[index] + " and " + parent);
+            arr[index] = parent;
+            arr[(index - 1) / 2] = temp;
+            siftUp((index - 1) / 2);
+        }
     }
-    h.removeMax();
- }
+
+    // this should check and alter the tree after an item is deleted.
+
+    // 3 points
+
+    // pre condition: index is the index of the item that was just moved to the root
+    // of the heap
+    // post condition: the heap is re-heapafied
+    private void siftDown(int index) {
+        int leftC = arr[((2 * index) + 1)];
+        int rightC = arr[((2 * index) + 2)];
+        int temp = arr[index];
+
+        if (arr[index] < leftC && leftC > rightC) {
+            arr[index] = arr[((2 * index) + 1)];
+            arr[((2 * index) + 1)] = temp;
+            siftDown(((2 * index) + 1));
+        } else if (arr[index] < rightC && rightC > leftC) {
+            arr[index] = arr[((2 * index) + 2)];
+            arr[((2 * index) + 2)] = temp;
+            siftDown(((2 * index) + 2));
+        }
+    }
+
+    // 4 points for syntax conventions.
+    // main method to test your heap
+    public static void main(String[] args) {
+        int[] nums = { 10, 5, 6, 7, 1, 2 };
+        Heap h = new Heap();
+        for (int i = 0; i < nums.length; i++) {
+            h.add(nums[i]);
+        }
+        h.removeMax();
+    }
 
 }
-
